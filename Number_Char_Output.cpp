5dc18498285beb775caae590f825c72141bd7ea0
Cong Shen <shencong85@gmail.com>
2023-01-08 18:15:50 -0500

    Added number and character output example

---
+++ Number_Char_Output.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num1 = 70;
+    double num2 = 256.783;
+    char ch = 'A';
+
+    cout << num1 << endl;  // print integer
+    cout << num2 << endl;  // print double
+    cout << "Character " << ch << endl;  //print character
+
+    return 0;
+}
